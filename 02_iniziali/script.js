9c38cb1468d46282189e7f40b08098c762dadeca
mauroanastasi <mauroanastasi1@gmail.com>
2025-03-24 22:56:53 +0100

    [UPD] es 2

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,9 +4,21 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function getPrimaLettera(names) {
+    return names.map
+    (function(name) {
+        return name.charAt(0);
+    });
+}
 
+// bosun
+// function getPrimaLettera(names){
+//     return names.map(name => name.charAt(0));
+// }
 
 // Invoca la funzione qui e stampa il risultato in console
+const primaLettera = getPrimaLettera (names);
+console.log(primaLettera)
 
 
 
